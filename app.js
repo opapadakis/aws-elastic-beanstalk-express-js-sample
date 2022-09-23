4cab032cc831c8ccc488124e4d0f98a0dec27bfa
opapadakis <46782806+opapadakis@users.noreply.github.com>
2022-09-23 16:50:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('let me learn this'));
+app.get('/', (req, res) => res.send('let me learn this 5'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
